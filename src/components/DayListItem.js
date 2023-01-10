1b46286fddbc45a66d5c1aacd20e3c5becd22e2a
Jesse Giles <jesse.giles@telus.com>
2023-01-09 20:06:31 -0700

    created, styled, and tested story for DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+
+  let buttonClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+   });
+ 
+
+  return (
+    <li className={buttonClass} onClick={() => props.setDay(props.name)}>
+        <h2 className='text--regular'>{props.name}</h2>
+        <h3 className='text--light'>
+          {props.spots} spots remaining
+        </h3>
+    </li>
+  );
+}
