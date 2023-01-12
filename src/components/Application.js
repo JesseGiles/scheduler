2c0f88b2bbfba50897669ac3162496efc987cf3e
Jesse Giles <jesse.giles@telus.com>
2023-01-11 18:38:04 -0700

    moved seperate states into single object and changed prop references etc to work with changes

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,14 +47,16 @@ const appointments = {
 
 export default function Application(props) {
 
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: []
-  //   // appointments: {}
-  // })
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+    // appointments: {}
+  });
 
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => { setState(prev => ({ ...prev, days }))}
 
   useEffect(() => {
     axios.get('/api/days')
@@ -84,8 +86,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
