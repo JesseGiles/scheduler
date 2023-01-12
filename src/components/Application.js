13fc96fc82c9682a3f40964bd785c403ee393992
Jesse Giles <jesse.giles@telus.com>
2023-01-11 18:22:09 -0700

    created helper/selectors function and tested, updated application.js to use axios for day data instead of a hardcoded array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -14,7 +15,7 @@ const appointments = {
     time: "1pm",
     interview: {
       student: "Lydia Miller-Jones",
-      interviewer:{
+      interviewer: {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
@@ -30,7 +31,7 @@ const appointments = {
     time: "3pm",
     interview: {
       student: "Archie Andrews",
-      interviewer:{
+      interviewer: {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
@@ -46,74 +47,32 @@ const appointments = {
 
 export default function Application(props) {
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: []
+  //   // appointments: {}
+  // })
 
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => {
+        setDays(response.data);
+      });
+  }, []);
 
-const listAppointments = Object.values(appointments).map((appt) => {
-  return (
-    <Appointment
-      key = {appt.id}
-      {...appt}
-    />
-  );
-}) 
-  
+
+  const listAppointments = Object.values(appointments).map((appt) => {
+    return (
+      <Appointment
+        key={appt.id}
+        {...appt}
+      />
+    );
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -124,11 +83,11 @@ const listAppointments = Object.values(appointments).map((appt) => {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={days}
-          value={day}
-          onChange={setDay}
-        />
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -138,7 +97,7 @@ const listAppointments = Object.values(appointments).map((appt) => {
       </section>
       <section className="schedule">
         {listAppointments}
-      <Appointment key="last" time="5pm" />
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredByDay = state.days.filter(date => date.name === day);
+  const todaysAppts = filteredByDay[0];
+
+  if (!todaysAppts) {
+    return [];
+  } 
+
+  const filteredAppointments = todaysAppts.appointments.map(appt => { 
+    return state.appointments[appt]
+  })
+
+  return filteredAppointments;
+}
