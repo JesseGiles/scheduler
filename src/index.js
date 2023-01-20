f273b605dbce4f32fe834f3f55d7d2355e7aa4f6
Jesse Giles <jesse.giles@telus.com>
2023-01-19 20:05:21 -0700

    created netlify.toml config file and set build environment to railway app url, updated index.js to ensure axios only uses base url value when it exists

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import "index.scss";
 
 import Application from "components/Application";
 
+// configure the client project to only use the base URL value when it exists as part of railway api server stretch
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(<Application />, document.getElementById("root"));
